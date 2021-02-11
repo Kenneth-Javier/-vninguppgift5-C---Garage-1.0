a01ae4654711aea494ee187d4a834a9c37a95e9c
Kenneth-Javier <kj.ortigoza@gmail.com>
2021-02-11 01:19:50 +0100

    Add project files.

---
+++ Övninguppgift5/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övninguppgift5
+{
+    class Car : IVehicle
+    {
+        public void Color()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnergySourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NumberOfWeels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Passengers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TransportationOn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VehicleRegistrationNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Övninguppgift5/IVehicle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övninguppgift5
+{
+    interface IVehicle :IEnumerable
+    {
+
+        //● Registreringsnumret är unikt
+        public void VehicleRegistrationNumber();
+        public void Color();
+        public void NumberOfWeels();
+        public void EnergySourse();
+        public void TransportationOn();
+
+        public void Passengers();
+
+        //        Kravspecifikation
+        //Fordonen ska implementeras som klassen Vehicle och subklasser till den.
+        //● Vehicle innehåller samtliga egenskaper som ska finnas i samtliga fordonstyper.
+
+        //T.ex.
+
+        //registreringsnummer, 
+        //färg, 
+        //antalhjul 
+     //och andra egenskaper ni kan komma på.
+        //energikälla
+        //färdmedel på
+
+
+        //● Det måste minst finnas följande subklasser:
+
+        //○ Airplane
+        //○ Motorcycle
+        //○ Car
+        //○ Bus
+        //○ Boat
+        //● Dessa skall implement
+    }
+}
